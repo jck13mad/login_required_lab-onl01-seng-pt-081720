50b0e988b12795c1a6ec83738e175a9b66814083
Jack Madden <jck13mad@gmail.com>
2021-02-20 22:52:59 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,12 @@ class SessionsController < ApplicationController
   end
   
   def create 
-    
+    if params[:name].present?
+      session[:name] = params[:name]
+      redirect_to '/'
+    else
+      redirect_to :login
+    end
   end 
   
   def destroy 
